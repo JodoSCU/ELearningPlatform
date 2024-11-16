310819da1485e1840c04ba07ff82fa4459de44fa
PatPrograming <30586247+PatPrograming@users.noreply.github.com>
2024-11-16 15:35:28 -0800

    changes made to add admin and emp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
+import EmployeePage from './pages/EmployeePage';
+import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
+    <><UserProvider>
+      <Router>
+        <Switch>
+          <Route path="/employee" component={EmployeePage} />
+          <Route path="/admin" component={AdminPage} />
+          <Route path="/" component={EmployeePage} />
+        </Switch>
+      </Router>
+    </UserProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div></>
   );
 }
 
