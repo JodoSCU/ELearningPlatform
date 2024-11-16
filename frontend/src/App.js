0412c89efa759bfb0294277a7ea0946f622aef9b
i-r-e-n-e <85856587+i-r-e-n-e@users.noreply.github.com>
2024-11-16 15:27:01 -0800

    signout button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react'; // This is the wrapper that provides authentication UI
+import { signOut } from 'aws-amplify/auth';
+import { Button } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the E-learning Platform</h1>
       <p>Build your personalized learning path</p>
+      <Button onClick={signOut}>
+        signout
+      </Button>
+
+
+      
     </div>
+
   );
 }
 
