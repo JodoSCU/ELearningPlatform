871711877e5ae812599670e680fb85ac0085e28e
PatPrograming <30586247+PatPrograming@users.noreply.github.com>
2024-11-16 16:13:08 -0800

    fixed app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { withAuthenticator } from '@aws-amplify/ui-react'; // This is the wrappe
 import { signOut } from 'aws-amplify/auth';
 import { Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import EmployeePage from './pages/EmployeePage';
@@ -11,34 +10,32 @@ import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
-    <><UserProvider>
+    <UserProvider>
       <Router>
-        <Switch>
-          <Route path="/employee" component={EmployeePage} />
-          <Route path="/admin" component={AdminPage} />
-          <Route path="/" component={EmployeePage} />
-        </Switch>
+        <div className="App"> {/* This div wraps everything inside UserProvider */}
+          <h1>Welcome to the E-learning Platform</h1>
+          <p>Build your personalized learning path</p>
+          <Button onClick={signOut}>
+            signout
+          </Button>
+          <Button>
+            Python
+          </Button>
+          <Button>
+            React
+          </Button>
+          <Button>
+            AWS Services
+          </Button>
+          
+          <Switch>
+            <Route path="/employee" component={EmployeePage} />
+            <Route path="/admin" component={AdminPage} />
+            <Route path="/" component={EmployeePage} />
+          </Switch>
+        </div>
       </Router>
     </UserProvider>
-    <div className="App">
-      <h1>Welcome to the E-learning Platform</h1>
-      <p>Build your personalized learning path</p>
-      <Button onClick={signOut}>
-        signout
-      </Button>
-      <Button>
-        Python
-      </Button>
-      <Button>
-        React
-      </Button>
-      <Button>
-        AWS Services
-      </Button>
-
-
-      
-    </div>
   );
 }
 
